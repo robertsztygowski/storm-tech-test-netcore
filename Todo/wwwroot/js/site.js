0baa7a82b426eafc110836e8302f47713793c7e8
Robert Sztygowski <robert@meniga.com>
2020-03-01 23:07:41 +0100

    Task 5. Added checkbox to allow show/hide done items

--- Todo/wwwroot/js/site.js
+++ Todo/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function todoItemIsDoneCheckbox() {
+    var hideDoneItemsFromUser = $("#isDoneCheckbox").prop('checked');
+    $('li[name="todoItem"]').each(function () {
+        var isItemDone = this.getAttribute('data-isdone').toLowerCase() == 'true';
+        if (isItemDone && hideDoneItemsFromUser) {
+            $(this).hide(200);
+        } else {
+            $(this).show(200);
+        }
+    });
+} 
